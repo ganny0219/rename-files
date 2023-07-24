dd4e218eb0b4aa6e9348c719701120f7db3c3d0e
GANNY <ganny0219@gmail.com>
2023-07-24 17:10:25 +0700

    asdasdasdasdasd

--- pages/api/rename.js
+++ pages/api/rename.js
@@ -36,7 +36,7 @@ router.use(uploadFile).post(async (req, res) => {
 
   const auth = await google.auth.getClient({
     scopes: ["https://www.googleapis.com/auth/drive"],
-    keyFile: path.join(process.cwd(), CREDEN),
+    keyFile: path.join(process.cwd(), process.env.CREDEN),
   });
 
   const driver = google.drive({ version: "v3", auth });
@@ -49,7 +49,7 @@ router.use(uploadFile).post(async (req, res) => {
   await driver.files.create({
     requestBody: {
       name: "loveyou.zip",
-      parents: [DRIVE_ID],
+      parents: [process.env.DRIVE_ID],
     },
     media: {
       mimeType: "application/zip",
