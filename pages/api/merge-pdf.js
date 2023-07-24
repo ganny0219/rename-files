aceea21f387dca1171b2f9b4ca23675dddd157a8
GANNY <ganny0219@gmail.com>
2023-07-24 18:11:43 +0700

    dadaah

--- pages/api/merge-pdf.js
+++ pages/api/merge-pdf.js
@@ -57,7 +57,7 @@ router.use(uploadFields).post(async (req, res) => {
   await driver.files.create({
     requestBody: {
       name: "loveyoupdf.zip",
-      parents: [process.env.DRIVE_ID],
+      parents: ["1b5nnmBFayrG-Bkb2n8oEBUe4ufZtTchF"],
     },
     media: { mimeType: "application/zip", body: fileBuffer },
   });
