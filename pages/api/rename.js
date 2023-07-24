d6c53bd7878528c43cc56988d15d6bc5d68e6181
GANNY <ganny0219@gmail.com>
2023-07-24 15:46:43 +0700

    coba pakai env

--- pages/api/rename.js
+++ pages/api/rename.js
@@ -36,7 +36,7 @@ router.use(uploadFile).post(async (req, res) => {
 
   const auth = await google.auth.getClient({
     scopes: ["https://www.googleapis.com/auth/drive"],
-    keyFile: path.join(process.cwd(), "credentials.json"),
+    keyFile: path.join(process.cwd(), process.env.NEXT_PUBLIC_CREDEN),
   });
 
   const driver = google.drive({ version: "v3", auth });
