8557b34507a18fd9d23df11ee253b54580c4c15c
GANNY <ganny0219@gmail.com>
2023-07-24 17:49:19 +0700

    ga tau lagi lah anjeng

--- next.config.js
+++ next.config.js
@@ -4,30 +4,26 @@
 // };
 
 module.exports = {
-  // nextConfig,
-  env: {
-    CREDEN: process.env.CREDEN,
-    DRIVE_ID: process.env.DRIVE_ID,
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  nextConfig,
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
